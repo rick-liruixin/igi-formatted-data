6a9d6a4fcaf4e959657e31e91d330fb704577061
李瑞鑫 <15939942017@163.com>
2023-11-05 16:14:51 +0800

    formatted-data

---
+++ index.js
@@ -0,0 +1,9 @@
+export function formattedAxiosData(params) {
+  return params;
+}
+export function formattedData(params) {
+  return params;
+}
+export function formattedFunData(params) {
+  return params;
+}
