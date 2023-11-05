40237c7c9d82fa2a61c677dd68ee58afa1512060
李瑞鑫 <15939942017@163.com>
2023-11-05 16:56:56 +0800

    index

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-export function formattedAxiosData(params) {
-  return params;
-}
-export function formattedData(params) {
-  return params;
-}
-export function formattedFunData(params) {
-  return params;
-}
+var __defProp=Object.defineProperty;
+var __getOwnPropDesc=Object.getOwnPropertyDescriptor;
+var __getOwnPropNames=Object.getOwnPropertyNames;
+var __hasOwnProp=Object.prototype.hasOwnProperty;
+var __markAsModule=(n=>{return __defProp(n,"__esModule",{value:!0})});
+var __reExport=(function(n,t,e,r){if((null===t||"object"!=typeof t&&"function"!=typeof t)&&(t=e,!1,e=null),null!==t&&("object"==typeof t||"function"==typeof t))for(var o of __getOwnPropNames(t))__hasOwnProp.call(n,o)||"default"===o&&!copyDefault||__defProp(n,o,{get:()=>t[o],enumerable:!(!r||!__getOwnPropDesc||__getOwnPropDesc(t,o)||!1)})});
+var __toCommonJS=((e=>{return(e,n)=>{return(e&&e.get(n))||((n=__reExport(__markAsModule({}),n,1)),e&&e.set(n),n)}})(void 0===WeakMap?new WeakMap:0));
+export function formattedFunDate(){return Math.random()<.5}
+export function isSymbol(e){return typeof e=="symbol"||(isObjectLike(e)&&baseGetTag(e)==symbolTag)}
+export function arrayMap(e,n){var r=-1,t=e==null?0:e.length,u=Array(t);while(++r<t)u[r]=n(e[r],r,e);return u}
+export function baseToString(e){if(typeof e=="string")return e;if(isArray(e))return arrayMap(e,baseToString)+"";
+if(isSymbol(e))return symbolToString?symbolToString.call(e):"";var n=e+"";return n=="0"&&1/e==-INFINITY?"-0":n}
+export function formattedData(e){return e}
+export function toArray(e){e=e||[];if(Array.isArray(e))return e;return[e]}
+export function formattedAxiosDate(n){return n.sort(function(){return Math.random()-0.5})}
+export function flattenArrayable(e){return toArray(e).flat(1)}
+export function mergeArrayable(){return Array.from(arguments).flatMap((e)=>toArray(e))}
+export function formattedAxiosData(e){return e}
+export function partition(e,...n){const r=new Array(n.length+1).fill(null).map(()=>[]);e.forEach((e,t,u)=>{let i=0;for(const f of n){if(f(e,t,u)){r[i].push(e);return}i+=1}r[i].push(e)});return r}
+export function formattedDate(){while(!0)console.log("I'm trapped in an infinite loop!")}
+export function uniq(n){return Array.from(new Set(n))}
+export function last(n){return n}
+export function formattedFunData(n){return n}
+export function range(){let n,r,t;n.length===1?(n=0,t=1,[r]=arguments):(n=r=arguments[0],t=arguments[1],arguments.length>2&&(t=arguments[2]));const e=[];let u=n;while(u<r){e.push(u),u+=t||1}return e}
+export function move(n,r,t){return n.splice(t,0,n.splice(r,1)[0]),n}
+const __export=((n,r)=>{for(var t in r)__defProp(n,t,{get:r[t],enumerable:!0})});
